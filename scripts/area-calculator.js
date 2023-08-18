b66b976ef55994dd61f2fcacd53a61e9a7965f23
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-08-18 23:55:28 +0600

    some updates

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -7,6 +7,8 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     const area = (0.5 * baseValue * heightValue).toFixed(2);
 
     setTextValueByID('area-display', area);
+    
+    addToCalculationEntry('Triangle', area);
 })
 
 // Rectangle Area 
@@ -18,6 +20,8 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
     const area = (baseValue * heightValue).toFixed(2);
 
     setTextValueByID('rectangle-display', area);
+
+    addToCalculationEntry('Rectangle', area);
 })
 
 // Parallelogram Area
@@ -29,6 +33,9 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
     const area = (baseValue * heightValue).toFixed(2);
 
     setTextValueByID('parallelogram-display', area);
+
+    // add to calculation entry
+    addToCalculationEntry('Parallelogram', area);
 })
 
 // Ellipse Area
@@ -40,6 +47,8 @@ document.getElementById('btn-ellipse').addEventListener('click', function(){
     const area = (Math.PI * baseValue * heightValue).toFixed(2);
 
     setTextValueByID('ellipse-display', area);
+
+    addToCalculationEntry('Ellipse', area);
 })
 
 // reusable function
@@ -53,3 +62,35 @@ function setTextValueByID(elementId, value){
     const areaPlace = document.getElementById(elementId);
     areaPlace.innerText = value;
 }
+
+// Data Validation
+/**
+ * 1. Set the proper type validation of the input
+ * 2. check type using typeof
+ * 3. NaN means: Not a Number. isNan is checking wether the input is a number
+ */
+function inputValidOrNot(sideOne, sideTwo){
+    if(isNaN(sideOne) || isNaN(sideTwo) ){
+        alert('Please insert number');
+        return; 
+    }
+}
+
+// add to calculation entry
+ function addToCalculationEntry(areaType, area){
+     console.log(areaType+ ' ' + area + ' cm2');
+    //  get the element shere you want to add the dynamic HTML
+     const calculationEntry = document.getElementById('calculation-entry');
+
+    // create the element you want to add
+    const p = document.createElement('p');
+
+    // get how many child there are
+    const count = calculationEntry.childElementCount;
+
+    // set inner html
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="mr-4 btn btn-sm btn-success text-white">Convert</button>`;
+
+    // append the created element as the child of the parent
+    calculationEntry.appendChild(p);
+ }
